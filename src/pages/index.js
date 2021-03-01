d0abb404d19e9493c38dcd1caddf99c514094977
Kioshi Okamoto <kjor29@gmail.com>
2021-03-01 13:27:28 -0500

    :fire: IMPROVE: Add css global and local - add static image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
+import { Link } from "gatsby";
 import React from "react"
 import Layout from "../components/Layout"
+import styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>Web developer based in Lima</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
+        <img src="/banner.png" alt="banner" style={{maxWidth:'100%'}} />
       </section>
     </Layout>
   )
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 import Layout from "../../components/Layout"
-
+import styles from '../../styles/projects.module.css'
 const index = () => {
   return (
     <Layout>
-      <div>
+      <div className={styles.portfolio}>
         <h1>Portfolio</h1>
         <h3>Projects & Websites I've created</h3>
       </div>
